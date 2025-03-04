ae6aecc237ac2752bc64f4b9c5fab537c9f122db
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-03-04 21:49:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
+import os
+os.environ["HF_HUB_ENABLE_HF_TRANSFER"]='1'
+from huggingface_hub import snapshot_download
 from sentence_transformers import CrossEncoder
 
 class InferlessPythonModel:
     def initialize(self):
-        self.model = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-12-v2', max_length=512,device="cuda")
+        model_id = 'cross-encoder/ms-marco-MiniLM-L-12-v2'
+        snapshot_download(repo_id=model_id,allow_patterns=["*.safetensors"])
+        self.model = CrossEncoder(model_id, max_length=512,device="cuda")
 
     def infer(self, inputs):
         query = inputs["query"]
